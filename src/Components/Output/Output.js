cbd4ab1f3c5f800e8c1adffe6e9d88aab04c71d6
John Bowman <bowman@ualberta.ca>
2020-07-03 23:40:07 -0600

    Restore Error reporting in output panel.

--- src/Components/Output/Output.js
+++ src/Components/Output/Output.js
@@ -10,12 +10,24 @@ const ContainerConstructor = connect ((store) => ({workspaces: store.workspaces,
 const Output = ContainerConstructor((props) => {
     const currentWorkspace = workspaceInspector(props);
 
-    if (currentWorkspace.id !== null) {
+    if(currentWorkspace.id !== null) {
+        if(currentWorkspace.output.responseType === "ERROR") {
+            const errorText = (currentWorkspace.output.stderr !== "") ? currentWorkspace.output.stderr : currentWorkspace.output.errorText;
+            return (
+                <div className={cssStyle.outputElse}>
+                    <div>
+                        <h4> Error: </h4>
+                            <p> {errorText} </p>
+                    </div>
+                </div>
+            )
+        } else {
       let string = (currentWorkspace.output.isUpdated)? " ": "";
       return (
           <iframe id="outFrame" className={cssStyle.outputPreview} src={string + currentWorkspace.output.path}
                   width="100%" height="100%" title="outputFrame" frameBorder="0"/>
       )
+        }
     } else {
         return (
             <div className={cssStyle.outputPreview}> 
