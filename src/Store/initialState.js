c86c108d8fcf1cbeea1ce25f7f56124040ca4ec5
Pedram Emami <emami1@ualberta.ca>
2020-07-30 23:44:50 -0600

    Reformat the initialState.js and store.js files to be compliant with the most recent restyling.

--- src/Store/initialState.js
+++ src/Store/initialState.js
@@ -1,48 +1,48 @@
 const initialState = {
-    workspaces: [
-        {
-            id: 1,
-            name: {
-                lastAssigned: "workspace",
-                current: "workspace",
-            },
-            codeOption: {
-                checked: false,
-                disabled: false,
-            },
-            outputOption: {
-                checked: true,
-                disabled: false,
-            },
-            codeText: "",
-            outformat: "html",
-            output: {
-                responseType: null,
-                errorType: null,
-                errorText: null,
-                errorCode: null,
-                errorContent: null,
-                stdin: "",
-                stdout: "",
-                stderr: "",
-                entryExists: false,
-                isUpdated: false,
-                path: ""
-            },
-            corePanesDisplay: {
-                codePane: true,
-                outputPane: true
-            },
-        },
-
-    ],
-    selectedWorkspace: {
-        id: 1,
-        index: 0
+  workspaces: [
+    {
+      id: 1,
+      name: {
+        lastAssigned: "workspace",
+        current: "workspace",
+      },
+      codeOption: {
+        checked: false,
+        disabled: false,
+      },
+      outputOption: {
+        checked: true,
+        disabled: false,
+      },
+      codeText: "",
+      outformat: "html",
+      output: {
+        responseType: null,
+        errorType: null,
+        errorText: null,
+        errorCode: null,
+        errorContent: null,
+        stdin: "",
+        stdout: "",
+        stderr: "",
+        entryExists: false,
+        isUpdated: false,
+        path: ""
+      },
+      corePanesDisplay: {
+        codePane: true,
+        outputPane: true
+      },
     },
-    workspacePaneStatus: {
-        view: true
-    }
+
+  ],
+  selectedWorkspace: {
+    id: 1,
+    index: 0
+  },
+  workspacePaneStatus: {
+    view: true
+  }
 
 }
 
