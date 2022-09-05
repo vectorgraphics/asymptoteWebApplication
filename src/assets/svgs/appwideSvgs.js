89c298ecbe0bc1c4f8dac445ccb460c437fb47d9
Pedram Emami <emami1@ualberta.ca>
2022-09-05 13:44:37 -0600

    Restructure assets folder and remove some redundant files.

---
+++ src/assets/svgs/appwideSvgs.js
@@ -0,0 +1,61 @@
+import { SvgIcon } from "@material-ui/core";
+
+export function EraserSVG({className="", classes={}, ...props}) {
+  return (
+    <SvgIcon className={className} classes={classes}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883
+           0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373
+           12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373
+           416H150.628l-80-80 124.686-124.686z"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
+
+export function DeleteSVG({className="", classes={}, ...props}) {
+  return (
+    <SvgIcon className={className} classes={classes}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16
+            16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16
+            16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136
+            32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
+
+
+export function FontIcon({className="", classes={}, ...props}) {
+  return (
+    <SvgIcon className={className} classes={classes}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path
+          d="M432 416h-23.41L277.88 53.69A32 32 0 0 0 247.58 32h-47.16a32 32 0 0 0-30.3 21.69L39.41 416H16a16
+        16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16h-19.58l23.3-64h152.56l23.3
+        64H304a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM176.85 272L224
+        142.51 271.15 272z"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
+
+export function KeyboardIcon({className="", classes={}, ...props}) {
+  return (
+    <SvgIcon className={className} classes={classes}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+          d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0
+          3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9
+          7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
+
+
