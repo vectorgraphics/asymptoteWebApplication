97a5b3d7f172d07368c4de84296d2957eace148a
Pedram Emami <emami1@ualberta.ca>
2022-09-12 17:07:58 -0600

    - Apply arrow function syntax. - Update code with respect to the changes made to redux data model.

--- src/uis/AWA/Components/CodeModule/EditorPane/Editor.js
+++ src/uis/AWA/Components/CodeModule/EditorPane/Editor.js
@@ -1,9 +1,8 @@
 import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import {
-  idSelector, appResetSelector, themeSelector, cmCodeSelector,
-  editorKeyBindingSelector, editorReRenderSelector,
-  editorLineNumbersSelector, editorFontsizeSelector,
+  idSelector, appResetSelector, themeSelector, cmCurrentCodeSelector, editorKeyBindingSelector,
+  editorReRenderSelector, editorLineNumbersSelector, editorFontsizeSelector,
 } from "../../../../../store/selectors";
 import { enActionCreator } from "../../../../../store/workspaces";
 import { cmActionCreator } from "../../../../../store/codeModule";
@@ -12,9 +11,9 @@ import CodeMirror from "codemirror/src/codemirror";
 import "./codemirror/codemirror.css";
 import "./codemirror/theme.css";
 import "./codemirror/asymptote";
-import "./codemirror/keymaps/emacs";
-import "./codemirror/keymaps/sublime";
-import "./codemirror/keymaps/vim";
+import "./codemirror/keyMaps/emacs";
+import "./codemirror/keyMaps/sublime";
+import "./codemirror/keyMaps/vim";
 
 const useStyle = makeStyles((theme) => ({
   editor: {
@@ -29,11 +28,11 @@ const useStyle = makeStyles((theme) => ({
 }));
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%     EDITOR COMPONENT
-export function Editor(props) {
+export const Editor = (props) => {
   const locClasses = useStyle();
   const cmInstance = useRef(null);
   const id = useSelector(idSelector);
-  const code = useSelector(cmCodeSelector);
+  const currentCode = useSelector(cmCurrentCodeSelector);
   const editorLineNumber = useSelector(editorLineNumbersSelector);
   const editorReRender = useSelector(editorReRenderSelector);
   const editorKeyBinding = useSelector(editorKeyBindingSelector);
@@ -41,6 +40,25 @@ export function Editor(props) {
   const appRequest  = useSelector(appResetSelector);
   const selectedTheme = useSelector(themeSelector);
   const dispatch = useDispatch();
+
+  let fontSize = "14px";
+  switch (editorFontSize) {
+    case "small":
+      fontSize = "12px";
+      break;
+    case "medium":
+      fontSize = "16px";
+      break;
+    case "large":
+      fontSize = "18px";
+      break;
+    case "default":
+      fontSize = "14px";
+      break;
+      default:
+      fontSize = "14px";
+      break;
+  };
 
   useEffect(() => {
     cmInstance.current = CodeMirror.fromTextArea(document.getElementById("tmpTextarea"), {
@@ -57,34 +75,31 @@ export function Editor(props) {
     if (editorKeyBinding !== "default") {
       cmInstance.current.setOption('keyMap', editorKeyBinding);
     }
-
+    cmInstance.current.getWrapperElement().style.fontSize = fontSize;                  // Setting fontSize each time the "editorKeyBinding" changes
     return () => cmInstance.current.toTextArea();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [editorKeyBinding, editorLineNumber, selectedTheme]);
+  }, [editorKeyBinding, editorLineNumber]);
 
   useEffect(() => {
-    let fontSize = "";
-    switch (editorFontSize) {
-      case "small":
-        fontSize = "12px"
-        break;
-      case "medium":
-        fontSize = "16px"
-        break;
-      case "large":
-        fontSize = "18px"
-        break;
-      default:
-        fontSize = "14px"
-        break;
+    cmInstance.current.getWrapperElement().style.fontSize = fontSize;                                      // Setting the awaReborn "selectedTheme" as the Codemirror theme
+    if (selectedTheme === "darkTheme") {
+      cmInstance.current.getWrapperElement().classList.remove("cm-s-lightTheme");
+      cmInstance.current.getWrapperElement().classList.add("cm-s-darkTheme");
+    } else {
+      cmInstance.current.getWrapperElement().classList.remove("cm-s-darkTheme");
+      cmInstance.current.getWrapperElement().classList.add("cm-s-lightTheme");
     }
+  }, [selectedTheme]);
+
+
+  useEffect(() => {                                                                   // Changing the editor fontSize
     cmInstance.current.getWrapperElement().style.fontSize = fontSize;
     dispatch(enActionCreator.reRenderEditor(id, editorReRender + 1));
   }, [editorFontSize]);
 
   useEffect(() => {
     function txtToStore(cm) {
-      dispatch(cmActionCreator.setCode(id, cm.getValue()));
+      dispatch(cmActionCreator.setCurrentCode(id, cm.getValue()));
     }
     cmInstance.current.on('change', txtToStore);
     return () => cmInstance.current.off('change', txtToStore);
@@ -92,7 +107,7 @@ export function Editor(props) {
   }, [id]);
 
   useEffect(() => {
-    cmInstance.current.setValue(code.currentContent);
+    cmInstance.current.setValue(currentCode);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [appRequest, editorReRender]);
 
@@ -101,4 +116,4 @@ export function Editor(props) {
       <textarea id="tmpTextarea" className={locClasses.textarea} autoFocus={true}/>
     </div>
   );
-}
+};
