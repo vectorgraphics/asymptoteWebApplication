4a16f40e0b3ff88b5a8116252e0ea1d1788ff986
Pedram Emami <emami1@ualberta.ca>
2022-09-12 16:41:06 -0600

    - Apply arrow function syntax.

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,22 +1,22 @@
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%  NAME & IDENTIFIER VALIDATOR
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-export function isValidName(name) {
+export const isValidName = (name) => {
   const emptyNameRegexp = /^\s*$/ig;
   const validNameRegexp = /^[^\d.][\w\d_]*$/ig;
   return (emptyNameRegexp.test(name))? false: validNameRegexp.test(name);
-}
+};
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  UNIQUE NAME VALIDATOR
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-export function isUniqueName(name, nameCollection=[]) {
+export const isUniqueName = (name, nameCollection=[]) => {
   const regExp = new RegExp(name, "g");
   return (nameCollection.length === 0)? true: !nameCollection.some((item) => regExp.test(item));
-}
+};
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ASY PAIR VALIDATOR
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-export function isValidAsyPair(value) {
+export const isValidAsyPair = (value) => {
   const regexp = /\s*\(\s*\S+\s*,\s*\S+\s*\)\s*/ig;
   return regexp.exec(value);
-}
+};
 
